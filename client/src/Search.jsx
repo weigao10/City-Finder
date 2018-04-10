7ad6c873e7f69da198c99d458f18a3992325b4ea
Wei Gao <wei.gao42@gmail.com>
2018-04-10 13:54:32 -0400

    added Search and Results components to client

---
+++ client/src/Search.jsx
@@ -0,0 +1,27 @@
+import react from 'react';
+import axios from 'axios';
+
+class Search extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      region: '*',
+      avg_high_temp: '*',
+      avg_rent: '*',
+      by_ocean: '*',
+      by_mountains: '*',
+      by_lake: '*',
+      population: '*'
+    }
+  }
+
+  render(){
+
+  }
+
+  onToggle(){
+
+  }
+}
+
+export default Search;
