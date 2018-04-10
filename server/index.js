d92ad854e9af84c9949eb4fa9b2a3f3ec062dbd4
Tom Wagner <wagne868@umn.edu>
2018-04-10 15:05:58 -0400

    Refactor server set up

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const helpers = require(path.join(__dirname + '/database/helpers.js'));
+const DB = require(path.join(__dirname + '/database/helpers.js'));
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(bodyParser.json());
 
-app.listen(PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('server listening on 3000!')
 })
 
-// console.log(path.join(__dirname, '/client/dist'));
