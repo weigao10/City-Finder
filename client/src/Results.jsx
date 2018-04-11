4c8944ebd243a77bb8ea977ebb6db15f353a69c0
Ryan Schabel <schabel12@gmail.com>
2018-04-10 20:56:30 -0400

    initial render results function

--- client/src/Results.jsx
+++ client/src/Results.jsx
@@ -6,17 +6,23 @@ class Results extends React.Component{
     super(props)
   }
   render(){
-    return (
-      <div className = 'cities' onClick = {this.props.showFaves ? this.delete : this.save}>
-        {this.props.results.forEach((city) => {
-          <div city={city} style={`background-image: url(`+city.image_url+`)`}>
-          <h3>{city.city_name}, {city.state}</h3>
-          <a>Pop: {city.population}</a>
-          <a>$/month: {city.avg_rent}</a>
-          </div>
-        })}
-      </div>
-    )
+    if (this.props.cities){
+      return (
+        <div className = 'cities' onClick = {this.props.showFaves ? this.delete : this.save}>
+          {this.props.cities.map((city) => {
+            <div city={city} style={`background-image: url(`+city.image_url+`)`}>
+            <h3>{city.city_name}, {city.state}</h3>
+            <a>Pop: {city.population}</a>
+            <a>$/month: {city.avg_rent}</a>
+            </div>
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <div>nothing</div>
+      )
+    }
   }
 }
 
