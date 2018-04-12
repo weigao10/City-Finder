4ddc13a8055c6cf7d248014728b6ee22a94b8229
Tom Wagner <wagne868@umn.edu>
2018-04-12 17:41:50 -0400

    Redesign buttons

--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -54,44 +54,45 @@ class Search extends React.Component{
   render(){
     return (
       <div className="buttons">
-        <div>
+        <div className="buttonRow">
           <p className="button-title">Cost of Living:</p>
-          <button id="1" name="rent" value="low" onClick={(event) => {this.onToggle(event)}}>Low</button>    
-          <button id="2" name="rent" value="medium" onClick={(event) => {this.onToggle(event)}}>Med</button>
-          <button id="3" name="rent" value="high" onClick={(event) => {this.onToggle(event)}}>High</button>        
+          <button className="width-one-third five-px-pad" id="1" name="rent" value="low" onClick={(event) => {this.onToggle(event)}}>Low</button>    
+          <button className="width-one-third five-px-pad" id="2" name="rent" value="medium" onClick={(event) => {this.onToggle(event)}}>Med</button>
+          <button className="width-one-third five-px-pad" id="3" name="rent" value="high" onClick={(event) => {this.onToggle(event)}}>High</button>        
         </div>
-        <div>
+        <div className="buttonRow">
           <p className="button-title">Weather:</p>
-          <button id="4" name="climate" value="cold" onClick={(event) => {this.onToggle(event)}}>Cold</button>    
-          <button id="5" name="climate" value="mild" onClick={(event) => {this.onToggle(event)}}>Mild</button>
-          <button id="6" name="climate" value="hot" onClick={(event) => {this.onToggle(event)}}>Hot</button>
+          <button className="width-one-third five-px-pad" id="4" name="climate" value="cold" onClick={(event) => {this.onToggle(event)}}>Cold</button>    
+          <button className="width-one-third five-px-pad" id="5" name="climate" value="mild" onClick={(event) => {this.onToggle(event)}}>Mild</button>
+          <button className="width-one-third five-px-pad" id="6" name="climate" value="hot" onClick={(event) => {this.onToggle(event)}}>Hot</button>
         </div>
-        <div>
+        <div className="buttonRow">
           <p className="button-title">Region:</p>
-          <button id="7" name="region" value="Northeast" onClick={(event) => {this.onToggle(event)}}>Northeast</button>    
-          <button id="8" name="region" value="Southeast" onClick={(event) => {this.onToggle(event)}}>Southeast</button>
-          <button id="9" name="region" value="Midwest" onClick={(event) => {this.onToggle(event)}}>Midwest</button>
+          <button className="width-one-third five-px-pad" id="7" name="region" value="Northeast" onClick={(event) => {this.onToggle(event)}}>Northeast</button>    
+          <button className="width-one-third five-px-pad" id="8" name="region" value="Southeast" onClick={(event) => {this.onToggle(event)}}>Southeast</button>
+          <button className="width-one-third five-px-pad" id="9" name="region" value="Midwest" onClick={(event) => {this.onToggle(event)}}>Midwest</button>
           <br></br>
-          <button id="10" name="region" value="Southwest" onClick={(event) => {this.onToggle(event)}}>Southwest</button>
-          <button id="11" name="region" value="Rockies" onClick={(event) => {this.onToggle(event)}}>Rockies</button>
-          <button id="12" name="region" value="Pacific" onClick={(event) => {this.onToggle(event)}}>Pacific</button>
+          <button className="width-one-third five-px-pad" id="10" name="region" value="Southwest" onClick={(event) => {this.onToggle(event)}}>Southwest</button>
+          <button className="width-one-third five-px-pad" id="11" name="region" value="Rockies" onClick={(event) => {this.onToggle(event)}}>Rockies</button>
+          <button className="width-one-third five-px-pad" id="12" name="region" value="Pacific" onClick={(event) => {this.onToggle(event)}}>Pacific</button>
         </div>
-        <div>
+        <div className="buttonRow">
           <p className="button-title">Environment:</p>
-          <button id="13" name="by_ocean" value="TRUE" onClick={(event) => {this.onToggle(event)}}>Near ocean</button>    
-          <button id="14" name="by_mountains" value="TRUE" onClick={(event) => {this.onToggle(event)}}>In the mountains</button>
-          <button id="15" name="by_lake" value="TRUE" onClick={(event) => {this.onToggle(event)}}>Near major lake</button>
+          <button className="width-one-third five-px-pad" id="13" name="by_ocean" value="TRUE" onClick={(event) => {this.onToggle(event)}}>Near the ocean</button>    
+          <button className="width-one-third five-px-pad" id="14" name="by_mountains" value="TRUE" onClick={(event) => {this.onToggle(event)}}>In the mountains</button>
+          <button className="width-one-third five-px-pad" id="15" name="by_lake" value="TRUE" onClick={(event) => {this.onToggle(event)}}>Near major lake</button>
         </div>
-        <div>
-          <p className="button-title">Population:</p>
-          <button id="16" name="city_size" value="small" onClick={(event) => {this.onToggle(event)}}>Small city</button>
-          <button id="17" name="city_size" value="medium" onClick={(event) => {this.onToggle(event)}}>Mid-size city</button>
-          <button id="18" name="city_size" value="big" onClick={(event) => {this.onToggle(event)}}>Big city</button>
+        <div className="buttonRow">
+          <p className="button-title">Metro size:</p>
+          <button className="width-one-third five-px-pad" id="16" name="city_size" value="small" onClick={(event) => {this.onToggle(event)}}>Small</button>
+          <button className="width-one-third five-px-pad" id="17" name="city_size" value="medium" onClick={(event) => {this.onToggle(event)}}>Mid-size</button>
+          <button className="width-one-third five-px-pad" id="18" name="city_size" value="big" onClick={(event) => {this.onToggle(event)}}>Big</button>
         </div>
-        <button onClick = {this.clearFilters}>Show All Cities</button>
-        <div>
-          <br/><br/>
-          <button id="20" onClick={(event) => {this.displayOnPage(event)}}>Show Favorites</button>
+        <div className="buttonRow marginTop20">
+        <button className="one-hundred-percent" onClick = {this.clearFilters}>Show All Cities</button>
+        </div>
+        <div className="buttonRow marginTop20">
+        <button className="one-hundred-percent" id="20" onClick={(event) => {this.displayOnPage(event)}}>Show Favorites</button>
         </div>
       </div>
     )
